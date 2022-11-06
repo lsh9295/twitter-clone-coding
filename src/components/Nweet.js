38c72d878129a46a84f1d28ea8f7bcf957b15902
lsh9295 <lsh929500@gmail.com>
2022-11-07 02:39:41 +0900

    add: Firebase storage file upload

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
-import { dbService } from "mFirebase";
+import { dbService, storageService } from "mFirebase";
 import { doc, deleteDoc, updateDoc }from"firebase/firestore";
+import { deleteObject, ref } from "@firebase/storage";
 
 const Nweet = ({nweetObj, isOwner}) => {
     const [editing, setEditing] = useState(false);
     const [newNweet, setNewNweet] = useState(nweetObj.text);
-    const NweetTextRef =doc(dbService, "nweets", `${nweetObj.id}`);
-
+    const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+    const desertRef = ref(storageService, nweetObj.attachmentUrl);
+    
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this nweet?");
         if (ok) {
-            await deleteDoc(NweetTextRef);
+            try {
+                await deleteDoc(NweetTextRef);
+
+                if (nweetObj.attachmentUrl !== "") {
+                    await deleteObject(desertRef);
+                }
+                } catch (error) {
+                    window.alert("failed");
+            }
         }
     }
     const toggleEditing = () => setEditing((prev) => !prev);
@@ -31,13 +41,18 @@ const Nweet = ({nweetObj, isOwner}) => {
         <div>
         {editing ? (
             <>
-            <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Edit your Nweet" onChange={onChange} value={newNweet} required />
-                <input type="submit" value="Update Nweet"/>
-            </form>
-            <button onClick={toggleEditing}>Cancle</button>
+            {isOwner && (
+            <>
+                <form onSubmit={onSubmit}>
+                    <input type="text" placeholder="Edit your Nweet" onChange={onChange} value={newNweet} required />
+                    <input type="submit" value="Update Nweet"/>
+                </form>
+                <button onClick={toggleEditing}>Cancle</button>
+            </>
+            )}
             </>
         ) : <><h4>{nweetObj.text}</h4>
+            {nweetObj.attachmentUrl && <img src={nweetObj.attachmentUrl} width="50px" height="50px" />}
             {isOwner && (
                 <>
                     <button onClick={onDeleteClick}>Delete Nweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { dbService } from "mFirebase";
+import { dbService, storageService } from "mFirebase";
 import { addDoc, collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 import Nweet from "components/Nweet";
+
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState("");
+    
     useEffect(() => {
         const q = query(
             collection(dbService, "nweets"),
@@ -21,16 +26,26 @@ const Home = ({ userObj }) => {
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        try {
-            const docRef = await addDoc(collection(dbService, "nweets"), {
+        let attachmentUrl = "";
+        
+        if (attachment !== "") {
+            const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+            const response = await uploadString(fileRef, attachment, "data_url");
+            attachmentUrl = await getDownloadURL(response.ref);
+        }
+        const nweetObj = {
             text: nweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
-        });
+            attachmentUrl,
+        };
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), nweetObj);
         } catch (e) {
             console.error("Error adding document: ", e);
         }
         setNweet("");
+        setAttachment("");
         };
         const onChange = (event) => {
         const {
@@ -38,13 +53,33 @@ const Home = ({ userObj }) => {
         } = event;
         setNweet(value);
     };
-    console.log(nweets);
-    
+    const onFileChange = (event) => {
+        const {target: { files },
+    } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => {setAttachment("")}
+
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Nweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {nweets.map((nweet) => (
